b03574bcdc739429dc6deaba17b44b2e4e1539b6
aar2807 <aarnaagarwaldlh@gmail.com>
2025-06-21 15:49:50 +0530

    Add files via upload
    Chapter 2 of python done ✅

---
+++ variables& expressions.py
@@ -0,0 +1,29 @@
+#ch2_variables&expressions
+
+# Variable Examples
+name = "Aarna"
+age = 17
+college = "IPU"
+
+print(name)
+print(age)
+print(college)
+
+# Math Expressions
+a = 10
+b = 3
+
+print(a + b)   # Addition
+print(a - b)   # Subtraction
+print(a * b)   # Multiplication
+print(a / b)   # Division
+
+#input from user
+college = input("Which college will you join? ")
+print("You will rock at " + college + "!")
+
+#Type conversion
+percentage = float(input("What is your Class 12 percentage? "))
+print("Next year, you'll be at " + str(percentage + 2) + "%!")
+
+#Done!
